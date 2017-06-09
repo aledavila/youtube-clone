6dd86c0c92189c4a1208113edbc4001f83ec12be
Ale Davila <aledavila@ibm.com>
2017-06-09 09:43:19 -0500

    env setup

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+      extensions: ['.js', '.jsx']
+  }
+};
